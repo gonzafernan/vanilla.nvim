7b38fd74a4d8d5c568ca0780d3d68aa99d17e38f
Gonzalo G. Fernández <fernandez.gfg@gmail.com>
2024-01-02 23:10:35 -0300

    Added LSP support
    - Mason
    - Mason-lspconfig
    - Nvim-lspconfig
    - Telescope ui-select extension

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,34 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "bashls", "clangd", "jsonls", "texlab", "pyright" }
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+            lspconfig.bashls.setup({})
+            lspconfig.clangd.setup({})
+            lspconfig.jsonls.setup({})
+            lspconfig.texlab.setup({})
+            lspconfig.pyright.setup({})
+
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+    }
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,11 +5,10 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function () 
+    config = function ()
         local configs = require("nvim-treesitter.configs")
         configs.setup({
-            ensure_installed = { "c", "lua", "vim", "python" },
-            sync_install = false,
+            ensure_installed = { "c", "lua", "vim", "python", "markdown" },
             highlight = { enable = true },
             indent = { enable = true },
         })

7b6e43b7fabb46dba0729bbcb6c68de6c667bc12
Gonzalo G. Fernández <fernandez.gfg@gmail.com>
2024-01-03 19:31:23 -0300

    Added auto install to treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,10 +5,19 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function ()
+    config = function()
         local configs = require("nvim-treesitter.configs")
         configs.setup({
+            -- A list of parser names, or "all" (the five listed parsers should always be installed)
             ensure_installed = { "c", "lua", "vim", "python", "markdown" },
+
+            -- Install parsers synchronously (only applied to `ensure_installed`)
+            sync_install = false,
+
+            -- Automatically install missing parsers when entering buffer
+            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+            auto_install = true,
+
             highlight = { enable = true },
             indent = { enable = true },
         })

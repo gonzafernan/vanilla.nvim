da4bccf648b8e5876fc5313af4301aa3b40cfb6b
Gonzalo G. Fernández <fernandez.gfg@gmail.com>
2024-01-03 19:44:48 -0300

    Added none-ls (null-ls) for formatting and linting

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,22 @@
+-- Null-ls (deprecated): none-ls.nvim (https://github.com/nvimtools/none-ls.nvim)
+return {
+    {
+        "nvimtools/none-ls.nvim",
+        config = function()
+            local null_ls = require("null-ls")
+
+            null_ls.setup({
+                sources = {
+                    -- Lua
+                    null_ls.builtins.formatting.stylua,
+
+                    -- Python
+                    null_ls.builtins.formatting.black,
+                    null_ls.builtins.formatting.isort,
+                },
+            })
+
+            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+        end
+    },
+}

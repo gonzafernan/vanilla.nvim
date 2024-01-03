410217856b9ef2b49bc469ea0964fd16a9d02b3e
Gonzalo G. Fernández <fernandez.gfg@gmail.com>
2024-01-02 23:09:46 -0300

    Modified lualine theme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,3 +1,4 @@
+-- Lualine: luaine.nvim (https://github.com/nvim-lualine/lualine.nvim)
 return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
@@ -5,7 +6,7 @@ return {
         require('lualine').setup({
             options = {
                 icons_enabled = true,
-                theme = "dracula"
+                theme = "palenight"
             }
         })
     end

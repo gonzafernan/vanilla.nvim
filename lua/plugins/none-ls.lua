f527cba441ae241be38959d96f0f534e45e8d135
Gonzalo G. Fernández <fernandez.gfg@gmail.com>
2024-03-17 12:49:15 -0300

    Added autoformat on save

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,25 +1,35 @@
 -- Null-ls (deprecated): none-ls.nvim (https://github.com/nvimtools/none-ls.nvim)
 return {
-    {
-        "nvimtools/none-ls.nvim",
-        config = function()
-            local null_ls = require("null-ls")
+	{
+		"nvimtools/none-ls.nvim",
+		config = function()
+			local null_ls = require("null-ls")
 
-            null_ls.setup({
-                sources = {
-                    -- Lua
-                    null_ls.builtins.formatting.stylua,
-                    -- Python
-                    null_ls.builtins.formatting.black,
-                    null_ls.builtins.formatting.isort,
-                    -- C/C++
-                    null_ls.builtins.formatting.clang_format,
-                    -- YAML
-                    null_ls.builtins.formatting.yamlfmt,
-                },
-            })
-
-            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-        end,
-    },
+			null_ls.setup({
+				on_attach = function(client, bufnr)
+					if client.supports_method("textDocument/formatting") then
+						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+						vim.api.nvim_create_autocmd("BufWritePre", {
+							group = augroup,
+							buffer = bufnr,
+							callback = function()
+								vim.lsp.buf.format()
+							end,
+						})
+					end
+				end,
+				sources = {
+					-- Lua
+					null_ls.builtins.formatting.stylua,
+					-- Python
+					null_ls.builtins.formatting.black,
+					null_ls.builtins.formatting.isort,
+					-- C/C++
+					null_ls.builtins.formatting.clang_format,
+					-- YAML
+					null_ls.builtins.formatting.yamlfmt,
+				},
+			})
+		end,
+	},
 }

22a507917d779b23aa8d04d9c28f83454a101433
Gonzalo G. Fernández <fernandez.gfg@gmail.com>
2024-04-10 18:24:47 -0300

    Added noice and removed telescope browser

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -16,43 +16,6 @@ return {
 		end,
 	},
 	{
-		"nvim-telescope/telescope-file-browser.nvim",
-		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-		config = function()
-			require("telescope").setup({
-				defaults = {
-					theme = "center",
-					sorting_strategy = "ascending",
-					layout_config = {
-						horizontal = {
-							prompt_position = "top",
-							preview_width = 0.5,
-						},
-					},
-				},
-				extensions = {
-					file_browser = {
-						-- theme = "center",
-						-- disables netrw and use telescope-file-browser in its place
-						hijack_netrw = true,
-						mappings = {
-							["i"] = {
-								-- your custom insert mode mappings
-							},
-							["n"] = {
-								-- your custom normal mode mappings
-							},
-						},
-					},
-				},
-			})
-			-- To get ui-select loaded and working with telescope, you need to call
-			-- load_extension, somewhere after setup function:
-			require("telescope").load_extension("file_browser")
-			vim.keymap.set("n", "<leader>fj", ":Telescope file_browser<CR>")
-		end,
-	},
-	{
 		"nvim-telescope/telescope-ui-select.nvim",
 		config = function()
 			require("telescope").setup({

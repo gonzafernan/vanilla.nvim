8da0b2f9a821193ac606835dec6ee4c2508d827b
Gonzalo G. Fernández <fernandez.gfg@gmail.com>
2024-04-27 21:23:44 -0300

    Filtered written messages with noice

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -12,4 +12,18 @@ return {
 		--   If not available, we use `mini` as the fallback
 		"rcarriga/nvim-notify",
 	},
+	config = function()
+		require("noice").setup({
+			routes = {
+				{
+					filter = {
+						event = "msg_show",
+						kind = "",
+						find = "written",
+					},
+					opts = { skip = true },
+				},
+			},
+		})
+	end,
 }

10033ee8bd295963556968faeb0c6a387e75ec89
Gonzalo G. Fernandez <fernandez.gfg@gmail.com>
2024-12-20 14:27:13 +0100

    Remove mason and add lazydev I will start doing the installation of the necessary tools myself.

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,20 +1,19 @@
 return {
     {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls" }
-            })
-        end
-    },
-    {
         "neovim/nvim-lspconfig",
+        dependencies = {
+            {
+                "folke/lazydev.nvim",
+                ft = "lua", -- only load on lua files
+                opts = {
+                    library = {
+                        -- See the configuration section for more details
+                        -- Load luvit types when the `vim.uv` word is found
+                        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                    },
+                },
+            },
+        },
         lazy = false,
         config = function()
             local lspconfig = require("lspconfig")

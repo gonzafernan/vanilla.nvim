5510e06ce21dba0a35b576319dd1116469a99108
Gonzalo G. Fernandez <fernandez.gfg@gmail.com>
2025-09-14 23:15:30 +0200

    feat(lsp): add verilog lsp and formatter

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,32 +1,34 @@
 -- Null-ls (deprecated): none-ls.nvim (https://github.com/nvimtools/none-ls.nvim)
 return {
-	{
-		"nvimtools/none-ls.nvim",
-		commit = false,
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			local null_ls = require("null-ls")
-			local formatting = require("lsp.formatting")
+    {
+        "nvimtools/none-ls.nvim",
+        commit = false,
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local null_ls = require("null-ls")
+            local formatting = require("lsp.formatting")
 
-			null_ls.setup({
-				on_attach = formatting.on_attach,
-				sources = {
-					-- Lua
-					null_ls.builtins.formatting.stylua,
-					-- Python
-					null_ls.builtins.formatting.black,
-					null_ls.builtins.formatting.isort,
-					-- C/C++
-					null_ls.builtins.formatting.clang_format,
-					-- YAML
-					null_ls.builtins.formatting.yamlfmt,
-					-- Typst
-					null_ls.builtins.formatting.typstyle.with({
-						command = "typstyle",
-						filetypes = { "typst" },
-					}),
-				},
-			})
-		end,
-	},
+            null_ls.setup({
+                on_attach = formatting.on_attach,
+                sources = {
+                    -- Lua
+                    null_ls.builtins.formatting.stylua,
+                    -- Python
+                    null_ls.builtins.formatting.black,
+                    null_ls.builtins.formatting.isort,
+                    -- C/C++
+                    null_ls.builtins.formatting.clang_format,
+                    -- YAML
+                    null_ls.builtins.formatting.yamlfmt,
+                    -- Typst
+                    null_ls.builtins.formatting.typstyle.with({
+                        command = "typstyle",
+                        filetypes = { "typst" },
+                    }),
+                    -- Verilog
+                    null_ls.builtins.formatting.verible_verilog_format,
+                },
+            })
+        end,
+    },
 }

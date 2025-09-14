78d3425d38d447e5e804004448bfa061096d11bf
Gonzalo G. Fernandez <fernandez.gfg@gmail.com>
2025-09-14 22:47:55 +0200

    feat(lsp): add typst lsp and formatter configuration

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,28 +1,36 @@
 return {
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            {
-                "folke/lazydev.nvim",
-                ft = "lua", -- only load on lua files
-                opts = {
-                    library = {
-                        -- See the configuration section for more details
-                        -- Load luvit types when the `vim.uv` word is found
-                        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-                    },
-                },
-            },
-        },
-        lazy = false,
-        config = function()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{
+				"folke/lazydev.nvim",
+				ft = "lua", -- only load on lua files
+				opts = {
+					library = {
+						-- See the configuration section for more details
+						-- Load luvit types when the `vim.uv` word is found
+						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+					},
+				},
+			},
+		},
+		lazy = false,
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			-- tinymist configuration: https://myriad-dreamin.github.io/tinymist/frontend/neovim.html
+			lspconfig.tinymist.setup({
+				settings = {
+					formatterMode = "typstyle",
+					exportPdf = "onSave",
+					semanticTokens = "disable",
+				},
+			})
 
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-        end,
-    }
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,27 +1,32 @@
 -- Null-ls (deprecated): none-ls.nvim (https://github.com/nvimtools/none-ls.nvim)
 return {
-    {
-        "nvimtools/none-ls.nvim",
-        commit = false,
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            local null_ls = require("null-ls")
-            local formatting = require("lsp.formatting")
+	{
+		"nvimtools/none-ls.nvim",
+		commit = false,
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local null_ls = require("null-ls")
+			local formatting = require("lsp.formatting")
 
-            null_ls.setup({
-                on_attach = formatting.on_attach,
-                sources = {
-                    -- Lua
-                    null_ls.builtins.formatting.stylua,
-                    -- Python
-                    null_ls.builtins.formatting.black,
-                    null_ls.builtins.formatting.isort,
-                    -- C/C++
-                    null_ls.builtins.formatting.clang_format,
-                    -- YAML
-                    null_ls.builtins.formatting.yamlfmt,
-                },
-            })
-        end,
-    },
+			null_ls.setup({
+				on_attach = formatting.on_attach,
+				sources = {
+					-- Lua
+					null_ls.builtins.formatting.stylua,
+					-- Python
+					null_ls.builtins.formatting.black,
+					null_ls.builtins.formatting.isort,
+					-- C/C++
+					null_ls.builtins.formatting.clang_format,
+					-- YAML
+					null_ls.builtins.formatting.yamlfmt,
+					-- Typst
+					null_ls.builtins.formatting.typstyle.with({
+						command = "typstyle",
+						filetypes = { "typst" },
+					}),
+				},
+			})
+		end,
+	},
 }

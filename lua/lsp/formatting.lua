a7c1063564523453164f4b323d6f9ac54990e156
Gonzalo G. Fernandez <fernandez.gfg@gmail.com>
2025-11-21 20:57:01 +0100

    feat(lsp): replace python tools by ruff

--- lua/lsp/formatting.lua
+++ lua/lsp/formatting.lua
@@ -6,23 +6,33 @@ _G.disable_autoformat = false
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 M.on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-                if not _G.disable_autoformat then
-                    vim.lsp.buf.format({})
-                end
-            end,
-        })
-    end
+	if client.supports_method("textDocument/formatting") then
+		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = augroup,
+			buffer = bufnr,
+			callback = function()
+				if not _G.disable_autoformat then
+					vim.lsp.buf.format({})
+				end
+				vim.lsp.buf.format({
+					bufnr = bufnr,
+					async = false,
+					filter = function(c)
+						if vim.bo[bufnr].filetype == "python" then
+							return c.name == "ruff"
+						end
+						return true
+					end,
+				})
+			end,
+		})
+	end
 end
 
 M.toggle_autoformat = function()
-    _G.disable_autoformat = not _G.disable_autoformat
-    vim.notify("Autoformatting " .. (_G.disable_autoformat and "disabled" or "enabled"))
+	_G.disable_autoformat = not _G.disable_autoformat
+	vim.notify("Autoformatting " .. (_G.disable_autoformat and "disabled" or "enabled"))
 end
 
 return M
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,40 +1,52 @@
 return {
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            {
-                "folke/lazydev.nvim",
-                ft = "lua", -- only load on lua files
-                opts = {
-                    library = {
-                        -- See the configuration section for more details
-                        -- Load luvit types when the `vim.uv` word is found
-                        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-                    },
-                },
-            },
-        },
-        lazy = false,
-        config = function()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            -- tinymist configuration: https://myriad-dreamin.github.io/tinymist/frontend/neovim.html
-            lspconfig.tinymist.setup({
-                settings = {
-                    formatterMode = "typstyle",
-                    exportPdf = "onSave",
-                    semanticTokens = "disable",
-                },
-            })
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{
+				"folke/lazydev.nvim",
+				ft = "lua", -- only load on lua files
+				opts = {
+					library = {
+						-- See the configuration section for more details
+						-- Load luvit types when the `vim.uv` word is found
+						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+					},
+				},
+			},
+		},
+		lazy = false,
+		config = function()
+			local lspconfig = require("lspconfig")
+			local formatting = require("lsp.formatting")
 
-            lspconfig.verible.setup({
-                cmd = { "verible-verilog-ls" },
-            })
+			lspconfig.lua_ls.setup({
+				on_attach = formatting.on_attach,
+			})
+			lspconfig.ruff.setup({
+				on_attach = formatting.on_attach,
+			})
+			lspconfig.pyright.setup({
+				on_attach = formatting.on_attach,
+			})
+			-- tinymist configuration: https://myriad-dreamin.github.io/tinymist/frontend/neovim.html
+			lspconfig.tinymist.setup({
+				on_attach = formatting.on_attach,
+				settings = {
+					formatterMode = "typstyle",
+					exportPdf = "onSave",
+					semanticTokens = "disable",
+				},
+			})
 
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-        end,
-    },
+			lspconfig.verible.setup({
+				cmd = { "verible-verilog-ls" },
+				on_attach = formatting.on_attach,
+			})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
